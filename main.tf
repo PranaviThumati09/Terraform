426f624b0fa7011e8645f485968ed4128e2c7556
PranaviThumati09 <76991802+PranaviThumati09@users.noreply.github.com>
2023-01-04 21:34:53 -0500

    S3 bucket change

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = "us-west-2"
 }
+resource "aws_s3_bucket" "storage" {
+  bucket = "my-tf-test-bucket"
+}
 resource "aws_sns_topic" "user_updates" {
   name = "user-updates-topic"
 }
